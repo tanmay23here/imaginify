beb31c6da9e78900cca2fe639777169de338cc30
tanmay23here <patiltanmay547@gmail.com>
2024-04-09 21:53:39 +0530

    webhook

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
   publicRoutes: ["/api/webhooks/clerk"],
+  ignoredRoutes: ["/api/webhook/clerk"],
 });
  
 export const config = {
